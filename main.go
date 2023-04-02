bfdc2cccda6fedaace84180f62e4b10f2b6be418
Rafał Peisert <rafalmp@users.noreply.github.com>
2023-04-02 13:53:10 +0200

    Get answers from user

--- main.go
+++ main.go
@@ -33,7 +33,18 @@ func main() {
 	}
 
 	problems := parseRecords(records)
-	fmt.Println(problems)
+	correct := 0
+
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = ", i+1, p.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.answer {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
 }
 
 func parseRecords(records [][]string) (result []problem) {
